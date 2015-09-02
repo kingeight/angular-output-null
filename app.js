a2abe9401f28cd625a19cb3da376246d6b8e456e
kingeight <jiangnemo@gmail.com>
2015-09-02 12:04:38 +0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@
   )
   .controller('myCtrl',['$scope',function($scope){
   	$scope.mywords="abc";
-  	$scope.mywords1="":
+  	$scope.mywords1="";
   	$scope.mywords2=null;
   	$scope.mywords3=false;
   }]);
